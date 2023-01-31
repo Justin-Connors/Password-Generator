fba5445256494b2f43aef58c982f50f9f26e8a10
Justin <justinconnors@hotmail.ca>
2023-01-31 02:19:02 -0400

    cannot get password to take length

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var includeSymbols;
+var includeLowerCase;
+var includeUpperCase;
+var includeNumbers;
 
-var passwordLength = prompt(
-  "How many characters would you like your password to be? Between 8 and 128."
-);
-var passwordSymbols = prompt(
-  "Do you want to include special characters? Yes or No."
-);
-var includeLowerCase = prompt(
-  "Do you want to include lowercase letters? Yes or No."
-);
-var includeUpperCase = prompt(
-  "Do you want to include uppercase letters? Yes or No."
-);
-var includeNumbers = prompt("Do you want to include numbers? Yes or No.");
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var symbols = "!@#$%^&*(){}[]=<>/,.";
+
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
@@ -28,106 +24,62 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
   //Adding values to password
-  let lowerCase = "abcdefghijklmnopqrstuvwxyz";
-  let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  let numbers = "0123456789";
-  let symbols = "!@#$%^&*(){}[]=<>/,.";
   let password = "";
-  for (let i = 0; i < passwordLength; i++) {
-    if (includeSymbols = true) {
-      password += symbols.charAt(Math.floor(Math.random() * symbols.length));
-    }
-    if (includeLowerCase = true) {
+  userInput();
+  for (let i = 0; i <= password; i++) {
+    if (includeLowerCase == true) {
       password += lowerCase.charAt(Math.floor(Math.random() * lowerCase.length));
     }
-    if (includeUpperCase = true) {
+    if (includeUpperCase == true) {
       password += upperCase.charAt(Math.floor(Math.random() * upperCase.length));
     }
-    if (includeNumbers = true) {
+    if (includeNumbers == true) {
       password += numbers.charAt(Math.floor(Math.random() * numbers.length));
+    }
+    if (includeSymbols == true) {
+      password += symbols.charAt(Math.floor(Math.random() * symbols.length));
     }
   }
   return password;
 }
 
-//Making sure password length is between 8 and 128
-if (passwordLength >= 8 && passwordLength <= 128) {
-  password.length = parseInt(passwordLength);
-  console.log(password.length);
-} else if (passwordLength < 8 || passwordLength > 128) {
-  prompt("Password must be between 8 and 128 characters.");
-}
-
-//checking if user wants to include special characters
-if (
-  passwordSymbols.toLowerCase() === "yes" ||
-  passwordSymbols.toLowerCase() === "y"
-) {
-  includeSymbols = true;
-  // console.log(includeSymbols);
-} else if (
-  passwordSymbols.toLowerCase() === "no" ||
-  passwordSymbols.toLowerCase() === "n"
-) {
-  includeSymbols = false;
-} else {
-  prompt(
-    "Please answer with 'yes' or 'y', Or 'no' or 'n'. Special characters will be included."
+function userInput() {
+  var passwordLength = prompt(
+    "How many characters would you like your password to be? Between 8 and 128."
   );
-}
+    
+  //Making sure password length is between 8 and 128
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    password.length = parseInt(passwordLength);
+  } else if (passwordLength < 8 || passwordLength > 128) {
+    prompt("Password must be between 8 and 128 characters.");
+  }
 
-//checking if user wants to include lowercase letters
-if (
-  includeLowerCase.toLowerCase() === "yes" ||
-  includeLowerCase.toLowerCase() === "y"
-) {
-  includeLowerCase = true;
-  // console.log(includeLowerCase);
-} else if (
-  includeLowerCase.toLowerCase() === "no" ||
-  includeLowerCase.toLowerCase() === "n"
-) {
-  includeLowerCase = false;
-} else {
-  prompt(
-    "Please answer with 'yes' or 'y', Or 'no' or 'n'. Lowercase letters will be included."
-  );
-}
+  if (
+    confirm("Do you want to include special characters? Ok / Cancel.") == true
+  ) {
+    includeSymbols = true;
+  } else {
+    includeSymbols = false;
+  }
+  if (
+    confirm("Do you want to include lowercase letters? Ok / Cancel.") == true
+  ) {
+    includeLowerCase = true;
+  } else {
+    includeLowerCase = false;
+  }
+  if (
+    confirm("Do you want to include uppercase letters? Ok / Cancel.") == true
+  ) {
+    includeUpperCase = true;
+  } else {
+    includeUpperCase = false;
+  }
+  if (confirm("Do you want to include numbers? Ok / Cancel.") == true) {
+    includeNumbers = true;
+  } else {
+    includeNumbers = false;
+  }
 
-//checking if user wants to include uppercase letters
-if (
-  includeUpperCase.toLowerCase() === "yes" ||
-  includeUpperCase.toLowerCase() === "y"
-) {
-  includeUpperCase = true;
-  // console.log(includeUpperCase);
-} else if (
-  includeUpperCase.toLowerCase() === "no" ||
-  includeUpperCase.toLowerCase() === "n"
-) {
-  includeUpperCase = false;
-} else {
-  prompt(
-    "Please answer with 'yes' or 'y', Or 'no' or 'n'. Uppercase letters will be included."
-  );
 }
-
-//checking if user wants to include numbers
-if (
-  includeNumbers.toLowerCase() === "yes" ||
-  includeNumbers.toLowerCase() === "y"
-) {
-  includeNumbers = true;
-  // console.log(includeNumbers);
-} else if (
-  includeNumbers.toLowerCase() === "no" ||
-  includeNumbers.toLowerCase() === "n"
-) {
-  includeNumbers = false;
-} else {
-  prompt(
-    "Please answer with 'yes' or 'y', Or 'no' or 'n'. Numbers will be included."
-  );
-}
-
-generatePassword();
